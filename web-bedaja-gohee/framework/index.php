3cde5a1d67722c6d1f4ec55ef5fde0e8906602b9
EMBedaja <100077814+EMBedaja@users.noreply.github.com>
2023-02-19 22:44:25 +0800

    Add files via upload

---
+++ web-bedaja-gohee/framework/index.php
@@ -0,0 +1,51 @@
+<?php
+$page = (isset($_GET['page']) && $_GET['page'] != '') ? $_GET['page'] : '';
+$subpage = (isset($_GET['subpage']) && $_GET['subpage'] != '') ? $_GET['subpage'] : '';
+$action = (isset($_GET['action']) && $_GET['action'] != '') ? $_GET['action'] : '';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Your Application Name</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Assistant&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/custom.css?<?php echo time();?>">
+</head>
+<body>
+
+   <div id="container" class="container"> 
+   <div id="header">
+       <div class="navbar">
+           <h2> Application Name</h2>
+            <a href="index.php"> Home</a>
+           <a href="#">Link 1</a>
+             <a href="#">Link 2</a>
+            <a href="index.php?page=settings">Settings</a> 
+         <a href="#"><span>Log Out</span></a>
+       </div>
+        </div>
+        <div id="content">
+    <?php
+      switch($page){
+                case 'settings':
+                    require_once 'settings-module/index.php';
+                break; 
+                case 'module_two':
+                    require_once 'module-folder';
+                break; 
+                case 'module_xxx':
+                    require_once 'module-folder';
+                break; 
+                default:
+                    require_once 'main.php';
+                break; 
+            }
+    ?>
+  </div>
+</div>
+
+</body>
+</html>
---
+++ web-bedaja-gohee/framework/users-module/main.php
@@ -0,0 +1,29 @@
+<div id="subcontent">
+    <table id="data-list">
+      <tr>
+        <th>#</th>
+        <th>Name</th>
+        <th>Email</th>
+      </tr>
+      <tr>
+        <td>1</td>
+        <td>Maria Anders</td>
+        <td>maria.anders@email.com</td>
+      </tr>
+      <tr>
+        <td>2</td>
+        <td>Christina Berglund</td>
+        <td>christina.berglund@email.com</td>
+      </tr>
+      <tr>
+        <td>3</td>
+        <td>Francisco Chang</td>
+        <td>francisco.chang@email.com</td>
+      </tr>
+      <tr>
+        <td>4</td>
+        <td>Roland Mendel</td>
+        <td>roland.mendel@email.com</td>
+      </tr>
+    </table>
+</div>
