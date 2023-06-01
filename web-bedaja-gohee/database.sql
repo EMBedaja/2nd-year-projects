8517740d514f92353343af19f543353c608010e8
EMBedaja <100077814+EMBedaja@users.noreply.github.com>
2023-06-02 00:53:51 +0800

    Delete web-bedaja-gohee directory

--- web-bedaja-gohee/database.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS `tbl_product_inv`;
-CREATE TABLE `tbl_product_inv` (
-  `rec_id` int(8) NOT NULL default '0',
-  `prod_id` int(8) NOT NULL default '0',
-  `prod_qty` int(8) NOT NULL default '0',
-  KEY  (`prod_id`),
-  KEY (`rec_id`)
-) ENGINE=MYISAM;
